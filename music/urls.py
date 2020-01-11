738842ead6626aab0c92a71089d1c7d39b133de4
harsh jain <harsh9500@rediffmail.com>
2020-01-11 21:32:17 +0530

    Created Generic Views

--- music/urls.py
+++ music/urls.py
@@ -4,9 +4,7 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
+    url(r'^$',views.IndexView.as_view(),name='index'),
 
-    url(r'^(?P<album_id>[0-9]+)/$',views.detail,name='detail'),
-
-    url(r'^(?P<album_id>[0-9]+)/favorite/$',views.favorite,name='favorite'),
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,36 +1,35 @@
+from django.views import generic
 from django.shortcuts import render, get_object_or_404
 from .models import Album, Song
 
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
 
-def index(request):
-
-    all_albums=Album.objects.all()
-    context = {
-        'all_albums' : all_albums,
-    }
-    return render(request,'music/index.html',context)
-
-    # html = ""
-    # for album in all_albums:
-    #     url = "/music/" + str(album.id) + "/"
-    #     html += "<a href = " + url + ">" + album.album_title + "</a><br>"
-    # return HttpResponse(html)
+    def get_queryset(self):
+        return Album.objects.all()
+    
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
 
 
-def detail(request, album_id):
-    album = get_object_or_404(Album,pk=album_id)
-    return render(request,'music/detail.html',{"album":album})
+# def index(request):
 
-def favorite(request, album_id):
-    album = get_object_or_404(Album,pk=album_id)
-    try:
-        selected_song = album.song_set.get(id=request.POST['song'])
-    except (KeyError, SOng.DoesNotExist):
-        return render(request,'music/detail.html',{
-            "error_message":"You did not select a valid song",
-            "album":album,
-            })
-    else:
-        selected_song.is_favorite=True
-        selected_song.save()
-        return render(request,'music/detail.html',{"album":album})
+#     all_albums=Album.objects.all()
+#     context = {
+#         'all_albums' : all_albums,
+#     }
+#     return render(request,'music/index.html',context)
+
+#     # html = ""
+#     # for album in all_albums:
+#     #     url = "/music/" + str(album.id) + "/"
+#     #     html += "<a href = " + url + ">" + album.album_title + "</a><br>"
+#     # return HttpResponse(html)
+
+
+# def detail(request, album_id):
+#     album = get_object_or_404(Album,pk=album_id)
+#     return render(request,'music/detail.html',{"album":album})
+
