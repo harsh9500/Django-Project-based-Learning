abdcd90841d29a0f2564092e493ab8a6357661e1
harsh jain <harsh9500@rediffmail.com>
2020-01-12 12:48:28 +0530

    Completed User registration

--- music/urls.py
+++ music/urls.py
@@ -6,6 +6,8 @@ app_name = 'music'
 urlpatterns = [
     url(r'^$',views.IndexView.as_view(),name='index'),
 
+    url(r'^register/$',views.UserFormView.as_view(),name='register'),
+
     url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
 
     url(r'^album/add/$',views.AlbumCreate.as_view(),name='album-add'),
--- music/views.py
+++ music/views.py
@@ -1,8 +1,11 @@
 from django.views import generic
+from django.views.generic import View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Album, Song
+from django.contrib.auth import authenticate, login
+from .forms import UserForm
 
 
 class IndexView(generic.ListView):
@@ -32,6 +35,35 @@ class AlbumDelete(DeleteView):
     model = Album
     success_url = reverse_lazy('music:index')
 
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    def get(self,request):
+        form = self.form_class(None)
+        return render(request,self.template_name,{'form':form})
+
+    def post(self,request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username,password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request,user)
+                    return redirect('music:index')
+
+        return render(request,self.template_name,{'form':form})
+    
 # def index(request):
 
 #     all_albums=Album.objects.all()
