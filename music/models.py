9624f71df3068496a72d759c6dd2c3c3fdd31bd0
harsh jain <harsh9500@rediffmail.com>
2020-01-12 17:01:44 +0530

    Completed

--- music/models.py
+++ music/models.py
@@ -1,23 +1,25 @@
 from django.db import models
-from django.core.urlresolvers import reverse
+# from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Album(models.Model):
+    user = models.ForeignKey(User, default=1)
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
     album_logo = models.FileField()
+    is_favorite = models.BooleanField(default=False)
 
-    def get_absolute_url(self):
-        return reverse('music:detail', kwargs={'pk':self.pk})
+    # def get_absolute_url(self):
+    #     return reverse('music:detail', kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.album_title + " - " + self.artist
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+    audio_file = models.FileField(default='')
     is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
--- website/urls.py
+++ website/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^music/',include('music.urls')),
+    url(r'^',include('music.urls')),
 ]
 
 if settings.DEBUG:
