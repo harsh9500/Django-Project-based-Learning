2c16a03ee3a8856e4adba07718029192ffc08a24
harsh jain <harsh9500@rediffmail.com>
2020-01-12 11:31:10 +0530

    Added Bootstrap and Automatic form for creating new Album

--- music/urls.py
+++ music/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^$',views.IndexView.as_view(),name='index'),
 
     url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
+
+    url(r'^album/add/$',views.AlbumCreate.as_view(),name='album-add')
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,9 @@
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render, get_object_or_404
 from .models import Album, Song
+
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
@@ -9,9 +12,15 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Album.objects.all()
     
+
 class DetailView(generic.DetailView):
     model = Album
     template_name = 'music/detail.html'
+
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist','album_title','genre','album_logo']
 
 
 # def index(request):
@@ -22,14 +31,6 @@ class DetailView(generic.DetailView):
 #     }
 #     return render(request,'music/index.html',context)
 
-#     # html = ""
-#     # for album in all_albums:
-#     #     url = "/music/" + str(album.id) + "/"
-#     #     html += "<a href = " + url + ">" + album.album_title + "</a><br>"
-#     # return HttpResponse(html)
-
-
 # def detail(request, album_id):
 #     album = get_object_or_404(Album,pk=album_id)
 #     return render(request,'music/detail.html',{"album":album})
-
